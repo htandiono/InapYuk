d44bf85572d22e7a51f6b1134096c87847463762
Aries Setiawan <ariessetiawan2804@gmail.com>
2026-09-16 11:30:00 +0700

    chore: cleanup one-time test scripts and review diffs from sprint 5

--- test.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('./apps/api/src/generated/prisma/client');
-const prisma = new PrismaClient({ log: ['query'] });
-async function run() {
-  const rooms = await prisma.room.findMany();
-  console.log("Rooms:", rooms.length);
-  const found = await prisma.room.findFirst({ where: { id: rooms[0].id, deletedAt: null } });
-  console.log("Found:", !!found);
-}
-run();
